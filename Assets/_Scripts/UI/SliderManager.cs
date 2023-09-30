ebf2e50975557387e1ce6af54a16d5cce16b25cb
RenatoPodesta <renato.podesta@outlook.com>
2023-09-30 10:38:24 -0300

    DeletingSlideManager

--- Assets/_Scripts/UI/SliderManager.cs
+++ Assets/_Scripts/UI/SliderManager.cs
@@ -25,34 +25,4 @@ public class SliderManager : MonoBehaviour
         bar.transform.localScale = new Vector3(currentValue / maxValue, bar.transform.localScale.y, bar.transform.localScale.z);
     }
 
-    /*void Awake()
-    {
-        Actions.FindActionMap("UI").FindAction("Slider").performed += OnSliderUp;
-        Actions.FindActionMap("UI").FindAction("Slider").performed += OnSliderDown;
-    }
-
-    public void OnEnable()
-    {
-        Actions.FindActionMap("UI").Enable();
-    }
-
-    public void OnDisable()
-    {
-        Actions.FindActionMap("UI").Disable();
-    }
-
-    void ScaleBar()
-    {
-        bar.transform.localScale = new Vector3(currentValue / maxValue, bar.transform.localScale.y, bar.transform.localScale.z);
-    }
-
-    void OnSliderUp(InputAction.CallbackContext context)
-    {
-
-    }
-
-    void OnSliderDown(InputAction.CallbackContext context)
-    {
-
-    }*/
 }

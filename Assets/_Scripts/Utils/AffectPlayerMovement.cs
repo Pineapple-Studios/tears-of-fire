f103125df0c0e485156a5ec71dad80f10e5f5a1d
Eduardo Costa <eduhcosta@live.com>
2023-11-18 13:32:39 -0300

    Add force to move on platform

--- Assets/_Scripts/Utils/AffectPlayerMovement.cs
+++ Assets/_Scripts/Utils/AffectPlayerMovement.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
 public class AffectPlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    private bool _enabled = false;
+    private Rigidbody2D _target;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 3)
         {
-            collision.transform.parent = this.transform;
+            _target = collision.gameObject.GetComponent<Rigidbody2D>();
+            _enabled = true;
         }
     }
 
@@ -19,7 +31,25 @@ public class AffectPlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.layer == 3)
         {
-            collision.transform.parent = null;
+            _target = null;
+            _enabled = false;
         }
+    }
+
+    private void Update()
+    {
+        if (!_enabled || !_target) return;
+        if (_rb.velocity == Vector2.zero) return;
+
+        _target.velocity += _rb.velocity.y > 0 ? new Vector2(_rb.velocity.x, 0) : _rb.velocity;
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        // Inputs
+        Vector2 Direction = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+        _target.velocity += new Vector2(190 * Direction.x * Time.deltaTime, 0);
     }
 }
